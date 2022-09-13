96811ead244be7d0b08c973b5762af84bc5551eb
David-vargasV <4523@holbertonschool.com>
2022-09-13 17:06:01 -0500

    7_templates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
+from sre_parse import State
 from flask import Flask, render_template
 from models import storage
 
@@ -10,9 +11,8 @@ app = Flask(__name__)
 @app.route('/states_lis', strict_slashes=False)
 def states_list():
     """ State list """
-    all_states = storage.all('States')
-    all_states = [state for state in all_states.values()]
-    return(render_template('7-states_list.html', all_states=all_states))
+    states = list(storage.all(State).values())
+    return(render_template('7-states_list.html', states=states))
 
 
 @app.teardown_appcontext
@@ -22,4 +22,4 @@ def teardown(self):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
