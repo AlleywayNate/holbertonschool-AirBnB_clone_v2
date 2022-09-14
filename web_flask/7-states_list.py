959a928211a425d2c6da23b4d8036e1b0f9dd554
David-vargasV <4523@holbertonschool.com>
2022-09-14 15:33:38 -0500

    Updated_task_7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
-from models.state import State
 from flask import Flask, render_template
 from models import storage
 
@@ -11,15 +10,16 @@ app = Flask(__name__)
 @app.route('/states_lis', strict_slashes=False)
 def states_list():
     """ State list """
-    states = list(storage.all(State).values())
-    return(render_template('7-states_list.html', states=states))
+    states = storage.all("State")
+    states = [state for state in states.values()]
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
     """ Method to handle """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
